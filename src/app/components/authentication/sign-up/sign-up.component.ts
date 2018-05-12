4b8ec294840e3f891b64b464ef207317941f8053
JosueCubero <jo96cube@gmail.com>
2018-05-11 22:22:27 -0600

    Refactored

--- src/app/components/authentication/sign-up/sign-up.component.ts
+++ src/app/components/authentication/sign-up/sign-up.component.ts
@@ -10,16 +10,17 @@ import { UserService } from '../services/user.service';
 export class SignUpComponent implements OnInit {
 	// gets filled by the form in the html
 	newUserData: any = {};
+  confirmUserData: any = {};
 	triedToSignUp: boolean;
 	resultString: string = '';
-	
+
 	// inject a UserService instance to create the user
 	constructor(private userService: UserService) { }
-	
+
 	ngOnInit() {
 		this.triedToSignUp = false;
 	}
-	
+
 	signUp() {
 		this.triedToSignUp = true;
 		this.userService.create(this.newUserData).subscribe(
