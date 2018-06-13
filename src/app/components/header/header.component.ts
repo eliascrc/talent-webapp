882bb48b6dd3494cdf5ecb3826cf5095920637e9
Josue Leon Sarkis <jolesa97@gmail.com>
2018-06-13 00:54:01 -0600

    TAL-302 | FE | Logout

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {AuthenticateService} from '@services/authentication/authenticate.service';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  loggedIn = false;
+
+  constructor(private router: Router, private authenticateService: AuthenticateService) {
+  }
+
+  ngOnInit() {
+    this.authenticateService.isLoggedIn()
+      .then(response => {
+          this.loggedIn = response;
+        }
+      );
+  }
+
+  onLogout() {
+    this.authenticateService.logout().then(response => {
+      this.router.navigate(['/landing-page']);
+      location.reload();
+    });
+  }
+
+}
