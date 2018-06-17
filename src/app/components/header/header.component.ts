1af58a8ce916a68955fcd72ca19b57dedeb5505e
Josue Leon Sarkis <jolesa97@gmail.com>
2018-06-16 19:37:29 -0600

    Sidebar, header, user loggedin area almost finished

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {AuthenticateService} from '@services/authentication/authenticate.service';
 import {LocationStrategy} from '@angular/common';
+import {OrganizationService} from '@services/organization/organization.service';
 
 @Component({
   selector: 'app-header',
@@ -11,14 +12,34 @@ import {LocationStrategy} from '@angular/common';
 export class HeaderComponent implements OnInit {
 
   loggedIn = false;
+  displayHeader = false;
+  userOrganizationLogo: string;
+  userName: string;
+  displayLoggedInArea = false;
+  userProfilePicture: string;
 
-  constructor(private router: Router, private authenticateService: AuthenticateService, private url: LocationStrategy) {
+  constructor(private router: Router, private authenticateService: AuthenticateService, private organizationService: OrganizationService) {
   }
 
   ngOnInit() {
     this.authenticateService.isLoggedIn()
       .then(response => {
           this.loggedIn = response;
+          if (this.loggedIn) {
+            this.authenticateService.getLoggedInUserInfo().then(userInfo => {
+               const userInfoObject = JSON.parse(JSON.stringify(userInfo));
+               console.log(userInfoObject);
+               let name = userInfoObject.firstName;
+               name = name.concat(' ');
+               this.userName = name.concat(userInfoObject.lastName);
+               this.organizationService.getOrganization(userInfoObject.organization.uniqueIdentifier)
+                 .subscribe(userOrganization => {
+                   this.userOrganizationLogo = JSON.parse(JSON.stringify(userOrganization)).logo;
+                 });
+            });
+            this.userOrganizationLogo = '/assets/images/talent-logo.png';
+          }
+          this.displayHeader = true;
         }
       );
   }
@@ -32,6 +53,10 @@ export class HeaderComponent implements OnInit {
 
   onSignIn() {
       this.router.navigate(['/login']);
+  }
+
+  onLoggedInAreaChanged() {
+    this.displayLoggedInArea = !this.displayLoggedInArea;
   }
 
 }
