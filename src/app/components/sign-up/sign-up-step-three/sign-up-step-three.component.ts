2aeb83398cb3c64e2374e33b8ba71733aac719b8
Josue Leon Sarkis <jolesa97@gmail.com>
2018-06-29 01:55:29 -0600

    TAL-289 | FE | Sign Up Step 3 | Create Organization
    Form validation and responsive complete, only missing integration.

--- src/app/components/sign-up/sign-up-step-three/sign-up-step-three.component.ts
+++ src/app/components/sign-up/sign-up-step-three/sign-up-step-three.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
+import {SignupService} from '@services/sign-up/signup.service';
 
 @Component({
   selector: 'app-sign-up-step-three',
@@ -7,9 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpStepThreeComponent implements OnInit {
 
-  constructor() { }
+  private static DNS_REGEX: RegExp = new RegExp('^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9]))*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$');
+
+  formData = {
+    organizationName: '',
+    organizationId: ''
+  };
+
+  constructor(public router: Router, public signUpService: SignupService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    if (this.isOrganizationIdValid(form) && this.isNameNotEmpty(form)) {
+      this.formData.organizationName = form.value.organizationName.trim();
+      this.formData.organizationId = form.value.organizationIdentifier;
+      console.log('Successful fields');
+    }
+  }
+
+  isOrganizationIdValid(form: NgForm): boolean {
+    if (SignUpStepThreeComponent.DNS_REGEX.test(form.value.organizationId)) {
+      return true;
+    }
+    form.controls.organizationId.setErrors({'incorrect': true});
+    return false;
+  }
+
+  isNameNotEmpty(form: NgForm): boolean {
+    if (form.value.organizationName.trim() !== '') {
+      return true;
+    }
+    form.controls.organizationName.setErrors({'incorrect': true});
+    return false;
   }
 
 }
