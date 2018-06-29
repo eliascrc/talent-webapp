2fa60812499fabd35dbc34a5c035361edf6df521
renato2110 <renato.mainieri2110@gmail.com>
2018-06-29 01:44:37 -0600

    TAL-304 | FE | Accept Invite Screen
    Initial implementation of the design and control of the form.

--- src/app/components/authentication/accept-invite/accept-invite.component.ts
+++ src/app/components/authentication/accept-invite/accept-invite.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-accept-invite',
@@ -6,10 +8,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./accept-invite.component.css']
 })
 export class AcceptInviteComponent implements OnInit {
+formData = {
+    firstName: '',
+    lastName: '',
+    password: ''
+  };
 
-  constructor() { }
+  constructor(public router: Router) {
+
+  }
 
   ngOnInit() {
   }
 
+  /**
+   * Obtains the submitted form values and sends the request to the backend web service via the accept-invite service.
+   * @param {NgForm} form The form that the user modifies
+   */
+  onSubmit(form: NgForm) {
+    if (this.validatePassword(form)) {
+      this.formData.firstName = form.value.firstName;
+      this.formData.lastName = form.value.lastName;
+      this.formData.password = form.value.password;
+	  alert("Firstname: "+this.formData.firstName+
+			"\nLastname: "+this.formData.lastName+
+			"\nPassword: "+this.formData.password);
+    }
+  }
+
+  /**
+   * Validates if the password is safe enough and matches the confirmation
+   * @param {NgForm} form The form that the user modifies
+   * @returns {boolean} true if valid, false if not
+   */
+  validatePassword(form: NgForm) {
+    const isPasswordSafeEnough = this.isPasswordSafeEnough(form);
+    const passwordMatches = this.passwordsMatch(form);
+
+    if (!isPasswordSafeEnough) {
+      form.controls.password.setErrors({'incorrect': true});
+    } else {
+      form.controls.password.setErrors(null);
+    }
+
+    if (!passwordMatches) {
+      form.controls.confirm.setErrors({'incorrect': true});
+    } else {
+      form.controls.confirm.setErrors(null);
+    }
+
+    return isPasswordSafeEnough && passwordMatches;
+  }
+
+  /**
+   * Validates if the password has the security requirements of the back end
+   * @param {NgForm} form The form that the user modifies
+   * @returns {boolean} true if secure enough, false if not
+   */
+  isPasswordSafeEnough(form: NgForm) {
+    const password = form.value.password;
+    const symbols = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+
+    return password.length >= 8 && /\d/.test(password) && /[a-z]/.test(password) && /[A-Z]/.test(password) && symbols.test(password);
+  }
+
+  /**
+   * Validates if the password matches the password confirmation field
+   * @param {NgForm} form The form that the user modifies
+   * @returns {boolean} true if it matches, false if not
+   */
+  passwordsMatch(form: NgForm) {
+    return form.value.confirm === form.value.password;
+  }
 }
