e8d858fa44ea91fef78e6ae6684224f98e5db6ae
Elias Calderon <eliascr98@outlook.com>
2018-06-29 10:40:48 -0600

    TAL-254 FE | Sign Up Step 2 | Activate User Account
    Implemented the approved design.

--- src/app/components/sign-up/sign-up-step-two/sign-up-step-two.component.ts
+++ src/app/components/sign-up/sign-up-step-two/sign-up-step-two.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {NgForm} from '@angular/forms';
 
 /**
- * 
+ *
  */
 @Component({
   selector: 'app-sign-up-step-two',
@@ -10,9 +12,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpStepTwoComponent implements OnInit {
 
-  constructor() { }
+  email = 'marianaabellan@gmail.com';
+
+  constructor(public router: Router) {
+  }
 
   ngOnInit() {
+  }
+
+  /**
+   * Obtains the submitted form values and sends the request to the backend web service via the sign up service.
+   * @param {NgForm} form The form that the user modifies
+   */
+  onSubmit(form: NgForm) {
+
+  }
+
+  /**
+   * Redirects the user to the sign in page
+   */
+  onSignIn() {
+    this.router.navigate(['/login']);
+  }
+
+  validateField(event: any, position: number) {
+    const changedNumber = event.target.value;
+    let offset = 1;
+
+    if (changedNumber.length > 1) {
+      event.target.value = changedNumber[0];
+    }
+
+    if (event.target.value.length === 0) {
+      if (position === 0) {
+        offset = 0;
+      } else {
+        offset = -1;
+      }
+    }
+
+    const element = (document.getElementsByClassName('confirmation-number')[position + offset]) as HTMLElement;
+    console.log(element.innerHTML);
+    element.focus();
   }
 
 }
