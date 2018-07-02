7144f959efe33640cb4e947943fc5712056d07c8
majocubero <majo.qbro@gmail.com>
2018-07-01 23:59:43 -0600

    feature/Feature_TAL-365-My-Profile-Template
    edit button works!.

--- src/app/components/profile/user-profile/user-profile.component.ts
+++ src/app/components/profile/user-profile/user-profile.component.ts
@@ -25,39 +25,30 @@ export class UserProfileComponent implements OnInit {
    */
   ngOnInit() {
     this.userId = this.activatedRoute.snapshot.paramMap.get('userId');
-    if (this.userId !== this.getLoggedInUserId()) {
-      document.getElementById('edit-option').style.display = 'none' ;
-    }
     this.authenticateService.isLoggedIn()
       .then(response => {
           this.loggedIn = response;
           if (this.loggedIn) {
-            this.resourceService.getTechnicalResourceBasicInfoWithId(this.userId)
-              .then(userInfo => {
-                let name = userInfo.firstName;
-                name = name.concat(' ');
-                this.name = name.concat(userInfo.lastName);
-                this.userProfilePicture = userInfo.profilePicture;
-                this.userProfilePicture = userInfo.profilePicture.link;
-                //this.position = userInfo.technicalPosition;
-                console.log(this.userId);
-                console.log(userInfo.id);
-              }, error => {
-              });
+            this.authenticateService.getLoggedInUserInfo().then(loggedUserInfo => {
+              const loggedUserInfoObject = JSON.parse(JSON.stringify(loggedUserInfo));
+              const id = loggedUserInfoObject.id;
+              if (id !== this.userId) {
+                document.getElementById('edit-button').style.display = 'none';
+              }
+              this.resourceService.getTechnicalResourceBasicInfoWithId(this.userId)
+                .then(userInfo => {
+                  let name = userInfo.firstName;
+                  name = name.concat(' ');
+                  this.name = name.concat(userInfo.lastName);
+                  this.userProfilePicture = userInfo.profilePicture;
+                  this.userProfilePicture = userInfo.profilePicture.link;
+                  //this.position = userInfo.technicalPosition;
+                }, error => {
+                });
+            });
           }
         }
       );
-  }
-
-  /**
-   * Queries the authenticateService to get the loggedIn user id.
-   * @returns {any} loggedIn user id.
-   */
-  getLoggedInUserId(): any {
-    this.authenticateService.getLoggedInUserInfo().then(loggedUserInfo => {
-      const loggedUserInfoObject = JSON.parse(JSON.stringify(loggedUserInfo));
-      return loggedUserInfoObject.id;
-    });
   }
 
   // This method will be implemented when the edit profile component is ready
