8c187076195533ad6e882074b1f0169a15b1225f
Josue Leon Sarkis <jolesa97@gmail.com>
2018-07-08 01:08:39 -0600

    TAL-306 | FE | Dashboard Page
    Started working on it.

--- src/app/components/company-information/dashboard/dashboard.component.ts
+++ src/app/components/company-information/dashboard/dashboard.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {OrganizationService} from '@services/organization/organization.service';
+import {ProjectService} from '@services/project/project.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+/**
+ * Dashboard page for logged-in users. It displays the main search bar, the newest organization members and the list of
+ * active projects and their basic information.
+ *
+ * @author Josué León Sarkis
+ */
 export class DashboardComponent implements OnInit {
-	fullName: string = "";
-	
-	constructor() { }
 
-	ngOnInit() {
-		let loggedUser: any = JSON.parse(localStorage.getItem('loggedUser'));
-		if(loggedUser) {
-			this.fullName = loggedUser.firstName + ' ' + loggedUser.lastName;
-		} else {
-			this.fullName = null;
-		}
-	}
+  constructor(private router: Router, private organizationService: OrganizationService, private projectService: ProjectService) { }
+
+  ngOnInit() {
+
+  }
 
 }
